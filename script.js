5cc5c9008082b2c381da76afe81cd5031923c8da
VikasVerma-15 <97469119+VikasVerma-15@users.noreply.github.com>
2025-12-26 14:34:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,35 +1,27 @@
 const output = document.getElementById("output");
+const loadingDiv = document.getElementById("loading");
+const errorDiv = document.getElementById("error");
 
-const loadingDiv = document.createElement("div");
-loadingDiv.id = "loading";
-loadingDiv.innerText = "Loading images...";
-output.before(loadingDiv);
-
-const errorDiv = document.createElement("div");
-errorDiv.id = "error";
-errorDiv.style.color = "red";
-output.before(errorDiv);
-
-/* Image URLs */
 const images = [
   { url: "https://picsum.photos/id/237/200/300" },
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
 function downloadImage(url) {
   return new Promise((resolve, reject) => {
     const img = new Image();
 
     img.onload = () => resolve(img);
     img.onerror = () =>
-      reject(`Failed to download image: ${url}`);
+      reject("Failed to download image");
 
     img.src = url;
   });
 }
 
 function downloadImages() {
-  // Show loading spinner
+  // show loading
   loadingDiv.style.display = "block";
   errorDiv.innerText = "";
   output.innerHTML = "";
@@ -39,16 +31,17 @@ function downloadImages() {
   );
 
   Promise.all(promises)
-    .then((downloadedImages) => {
+    .then((imgs) => {
       loadingDiv.style.display = "none";
 
-      downloadedImages.forEach(img => {
+      imgs.forEach(img => {
         output.appendChild(img);
       });
     })
-    .catch((error) => {
+    .catch((err) => {
       loadingDiv.style.display = "none";
-      errorDiv.innerText = error;
+      errorDiv.innerText = err;
     });
 }
+
 downloadImages();
